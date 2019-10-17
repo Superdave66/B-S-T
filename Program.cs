25f7de9c6bc2a156bca0c93eba6e3bbb4b04950a
SPC David Wiliams II <davidwilliams1102@yahoo.com>
2019-10-17 07:30:30 -0500

    intial commit finshed thing

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinarySearchTree binarySearchTree = new BinarySearchTree();
+
+           binarySearchTree.Add(20);
+            binarySearchTree.Add(7);
+            binarySearchTree.Add(6);
+            binarySearchTree.Add(9);
+            binarySearchTree.Add(3);
+            binarySearchTree.Add(4);
+            binarySearchTree.Add(8);
+            binarySearchTree.Find(9);
+
+
+        }
+
+    }
+}
---
+++ binarySearchTree.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class BinarySearchTree
+    {
+        public Node Root { get; set; }
+        public bool Add(int value)
+        {
+            Node before = null, after = this.Root;
+            while(after != null)
+            {
+                before = after;
+                if (value < after.Data)
+                {
+                    after = after.leftNode;
+                }
+                else if (value > after.Data)
+                {
+                    after = after.RightNode;
+                }
+
+                else
+                {
+                    return false;
+                }
+
+            }
+            Node newNode = new Node();
+            newNode.Data = value;
+                if (this.Root== null)
+            {
+                this.Root = newNode;
+            }
+            else
+            {
+                if (value < before.Data)
+                {
+                    before.leftNode = newNode;
+                }
+                else
+                {
+                    before.RightNode = newNode;
+
+                }
+            }
+            return true;
+        }
+        public Node Find (int value )
+        {
+            return this.Find(value);
+            
+        }
+
+
+
+
+    }
+}
